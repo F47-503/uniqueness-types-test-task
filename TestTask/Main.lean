f1e4a5c0e5afc07e2f53c42a882bd41ecd90688b
Lean 4 VS Code Extension <>
2025-09-23 01:23:39 +0800

    Initial commit

---
+++ TestTask/Main.lean
@@ -0,0 +1,4 @@
+import TestTask
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ TestTask/TestTask.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `TestTask` library.
+-- Import modules here that should be built as part of the library.
+import TestTask.Basic
